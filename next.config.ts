dd482fe314b12ffd2c3931a6174b8b20bb8a8fe7
Shubham Tak <100914619+shub197@users.noreply.github.com>
2026-06-11 13:30:03 +0530

    next config file updated

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+    distDir: 'build',
     devIndicators: false,
     env: {
         NEXT_PUBLIC_API_ACCESS_TOKEN: process.env.NEXT_PUBLIC_API_ACCESS_TOKEN,
